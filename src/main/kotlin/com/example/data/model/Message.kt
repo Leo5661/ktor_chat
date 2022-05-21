023c667d9375f7389c0ba835915ac7ac3e1fd6f0
Leo5661 <singhra66@gmail.com>
2022-05-22 02:21:42 +0530

    ➕ Added MessageDataSource

--- src/main/kotlin/com/example/data/model/Message.kt
+++ src/main/kotlin/com/example/data/model/Message.kt
@@ -5,7 +5,7 @@ import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
 @Serializable
-data class Massage(
+data class Message(
     val text: String,
     val userName: String,
     val timeStamp: Long,
